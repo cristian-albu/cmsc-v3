375f9ae7bc53d24971c3f26fb39c4fe761b9ce02
Cristian Albu <albucristian95p@gmail.com>
2025-01-26 23:17:00 +0200

    linter errors

--- app/proiecte-si-programe/[slug]/page.tsx
+++ app/proiecte-si-programe/[slug]/page.tsx
@@ -1,23 +1,15 @@
 import client from "@/lib/client";
-import { GET_PROJECT_BY_SLUG, GET_PROJECTS_SLUGS } from "@/app/proiecte-si-programe/data/queries";
+import { GET_PROJECT_BY_SLUG } from "@/app/proiecte-si-programe/data/queries";
 import { T_BaseProject } from "@/app/proiecte-si-programe/data/queries/utils";
-
 import { notFound } from "next/navigation";
 import React from "react";
+import { getSlugs } from "./utils";
 
 type T_Params = { params: Promise<{ slug: string }> };
 
 type T_ProjectParams = {
   projectsProgramsCollection: { items: T_BaseProject[] };
 };
-
-export async function getSlugs() {
-  const { projectsProgramsCollection } = await client.request<{
-    projectsProgramsCollection: { items: { slug: string }[] };
-  }>(GET_PROJECTS_SLUGS);
-
-  return projectsProgramsCollection.items;
-}
 
 export async function generateStaticParams() {
   const slugs = await getSlugs();
