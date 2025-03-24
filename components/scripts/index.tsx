98b138695a25aa1c5f0c7dfb22b680e70c66ab7a
Cristian Albu <albucristian95p@gmail.com>
2025-03-25 00:56:07 +0200

    ga update

--- components/scripts/index.tsx
+++ components/scripts/index.tsx
@@ -1,34 +1,12 @@
 "use client";
 import { E_GdprNames, useGdprContext } from "@/lib/contexts/GdprContext";
-import Script from "next/script";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const Scripts = () => {
   const { gdprState } = useGdprContext();
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
-  return (
-    <>
-      {gdprState[E_GdprNames.analytics] && gaId && (
-        <>
-          <Script id="gaHead" async type="module" src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
-          <Script
-            id="gaBody"
-            type="module"
-            dangerouslySetInnerHTML={{
-              __html: `
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "${gaId}");
-`,
-            }}
-          />
-        </>
-      )}
-    </>
-  );
+  return <>{gdprState[E_GdprNames.analytics] && gaId && <GoogleAnalytics gaId={gaId} />}</>;
 };
 
 export default Scripts;
