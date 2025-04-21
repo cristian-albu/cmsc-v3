3873d0e06b6122ce2083160510cea6284480a952
Cristian Albu <albucristian95p@gmail.com>
2025-04-21 10:02:47 +0300

    added gtm (#17)

--- components/scripts/index.tsx
+++ components/scripts/index.tsx
@@ -1,12 +1,22 @@
 "use client";
 import { E_GdprNames, useGdprContext } from "@/lib/contexts/GdprContext";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const Scripts = () => {
   const { gdprState } = useGdprContext();
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "";
 
-  return <>{gdprState[E_GdprNames.analytics] && gaId && <GoogleAnalytics gaId={gaId} />}</>;
+  return (
+    <>
+      {gdprState[E_GdprNames.analytics] && gaId && (
+        <>
+          <GoogleAnalytics gaId={gaId} />
+          <GoogleTagManager gtmId={gtmId} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default Scripts;
